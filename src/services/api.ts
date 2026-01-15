484917121444f8ec05cb3c435353bc9419b387c4
Gustavo Pirani <gustavohpmendonca@gmail.com>
2026-01-15 12:21:10 -0400

    FEAT/ remoçao de url de busca desnecessárias

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,32 +1,9 @@
 import { TMDB_API_KEY, OMDB_API_KEY } from '@env';
 import axios from 'axios';
 
-// Busca filme pelo "nome" no TMDB
-export const apiNomeTMDB = axios.create({
-  baseURL: 'https://api.themoviedb.org/3',
-  params: {
-    api_key: TMDB_API_KEY,
-    language: 'pt-BR',
-    include_adult: false,
-  },
-  timeout: 5000,
-  headers: { 'Content-Type': 'application/json' },
-});
-
 // API principal do TMDB para buscar detalhes e credits
 export const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
-  params: {
-    api_key: TMDB_API_KEY,
-    language: 'pt-BR',
-  },
-  timeout: 5000,
-  headers: { 'Content-Type': 'application/json' },
-});
-
-// Busca dados extras do filme com base no "ID" do TMDB (manter para compatibilidade)
-export const apiIdTMDB = axios.create({
-  baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
     api_key: TMDB_API_KEY,
     language: 'pt-BR',
