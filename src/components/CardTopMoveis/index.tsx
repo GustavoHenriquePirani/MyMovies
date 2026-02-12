2fef2d4a74f5c21b27280b2bb5a5c5c3baabcca0
Gustavo Pirani <gustavohpmendonca@gmail.com>
2026-02-11 23:51:14 -0400

    feat: criaçao do CardTopMovies

---
+++ src/components/CardTopMoveis/index.tsx
@@ -0,0 +1,33 @@
+import { Pressable, Text, Image } from "react-native";
+import styles from "./styles";
+
+interface Movie {
+  id: number;
+  poster_path: string;
+}
+
+interface Props {
+  data: Movie;
+  onPress?: () => void;
+  position?: number;
+}
+
+export function CardTopMovies({ data, position, ...rest }: Props) {
+  return (
+    <Pressable {...rest} style={styles.cardTopMovies}>
+      <Image
+        style={styles.cardTopImage}
+        source={{
+          uri: `https://image.tmdb.org/t/p/w500${data.poster_path}`,
+        }}
+      />
+      {position && (
+        <Text
+          style={styles.numberPosition}
+        >
+          {position}
+        </Text>
+      )}
+    </Pressable>
+  );
+}
