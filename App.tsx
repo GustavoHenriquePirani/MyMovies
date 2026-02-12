20abf6668e63db5b9b004b944cc0e0252e73b05f
Gustavo Pirani <gustavohpmendonca@gmail.com>
2026-02-11 23:53:20 -0400

    fix: ajustes globais e nas dependencias

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
+import { FavoritesProvider } from "./src/contexts/FavoritesContext";
 import { StatusBar } from "expo-status-bar";
 import { Routes } from "./src/routes";
 import React from "react";
 
 export default function App() {
   return (
-    <>
+    <FavoritesProvider>
       <Routes />
       <StatusBar style="auto" />
-    </>
+    </FavoritesProvider>
   );
 }
